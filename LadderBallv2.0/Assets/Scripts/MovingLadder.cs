0cc5df2a1c5e9dee99259fd47c5677f90f8139ec
TomRichards14 <2richt38@solent.ac.uk>
2017-11-20 12:53:07 +0000

    Add files via upload

---
+++ LadderBallv2.0/Assets/Scripts/MovingLadder.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingLadder : MonoBehaviour {
+
+    public bool IsMovingLeft = true;
+    public bool IsMovingRight = false;
+
+    void Update()
+    {
+        if (IsMovingLeft == true)
+        {
+            MoveLeft();
+        }
+
+        if (IsMovingRight == true)
+        {
+            MoveRight();
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision Barrier)
+    {
+        if (IsMovingLeft == true)
+        {
+            IsMovingLeft = false;
+            IsMovingRight = true;
+        }
+        else if (IsMovingRight == true)
+        {
+            IsMovingRight = false;
+            IsMovingLeft = true;
+        }
+    }
+
+    void MoveLeft()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime);
+    }
+
+    void MoveRight()
+    {
+        transform.Translate(Vector3.back * Time.deltaTime);
+    }
+}
